4c1565d3c6287bb422162e73107f53229fad844d
Ibrahim-Karagaji <ibrahimKaragaji@gmail.com>
2024-12-04 01:47:06 +0300

    created the elemnt at js file

---
+++ First-training/json.js
@@ -0,0 +1,145 @@
+const titleAndDescription = [
+  {
+    title: "title",
+    Subject:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Itaque nisi tenetur maiores eos fugiat iste tempore delectus reprehenderit, ut ad! Harum culpa autem quae natus quod? Tempore facere dolorem fugiat?",
+  },
+  {
+    title: "title",
+    Subject:
+      "    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Itaque nisi tenetur maiores eos fugiat iste tempore delectus reprehenderit, ut ad! Harum culpa autem quae natus quod? Tempore facere dolorem fugiat?",
+  },
+  {
+    title: "title",
+    Subject:
+      "    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Itaque nisi tenetur maiores eos fugiat iste tempore delectus reprehenderit, ut ad! Harum culpa autem quae natus quod? Tempore facere dolorem fugiat?",
+  },
+  {
+    title: "title",
+    Subject:
+      "    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Itaque nisi tenetur maiores eos fugiat iste tempore delectus reprehenderit, ut ad! Harum culpa autem quae natus quod? Tempore facere dolorem fugiat?",
+  },
+  {
+    title: "title",
+    Subject:
+      "    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Itaque nisi tenetur maiores eos fugiat iste tempore delectus reprehenderit, ut ad! Harum culpa autem quae natus quod? Tempore facere dolorem fugiat?",
+  },
+  {
+    title: "title",
+    Subject:
+      "    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Itaque nisi tenetur maiores eos fugiat iste tempore delectus reprehenderit, ut ad! Harum culpa autem quae natus quod? Tempore facere dolorem fugiat?",
+  },
+  {
+    title: "title",
+    Subject:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Itaque nisi tenetur maiores eos fugiat iste tempore delectus reprehenderit, ut ad! Harum culpa autem quae natus quod? Tempore facere dolorem fugiat?",
+  },
+  {
+    title: "title",
+    Subject:
+      "    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Itaque nisi tenetur maiores eos fugiat iste tempore delectus reprehenderit, ut ad! Harum culpa autem quae natus quod? Tempore facere dolorem fugiat?",
+  },
+  {
+    title: "title",
+    Subject:
+      "    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Itaque nisi tenetur maiores eos fugiat iste tempore delectus reprehenderit, ut ad! Harum culpa autem quae natus quod? Tempore facere dolorem fugiat?",
+  },
+  {
+    title: "title",
+    Subject:
+      "    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Itaque nisi tenetur maiores eos fugiat iste tempore delectus reprehenderit, ut ad! Harum culpa autem quae natus quod? Tempore facere dolorem fugiat?",
+  },
+  {
+    title: "title",
+    Subject:
+      "    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Itaque nisi tenetur maiores eos fugiat iste tempore delectus reprehenderit, ut ad! Harum culpa autem quae natus quod? Tempore facere dolorem fugiat?",
+  },
+  {
+    title: "title",
+    Subject:
+      "    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Itaque nisi tenetur maiores eos fugiat iste tempore delectus reprehenderit, ut ad! Harum culpa autem quae natus quod? Tempore facere dolorem fugiat?",
+  },
+  {
+    title: "title",
+    Subject:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Itaque nisi tenetur maiores eos fugiat iste tempore delectus reprehenderit, ut ad! Harum culpa autem quae natus quod? Tempore facere dolorem fugiat?",
+  },
+  {
+    title: "title",
+    Subject:
+      "    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Itaque nisi tenetur maiores eos fugiat iste tempore delectus reprehenderit, ut ad! Harum culpa autem quae natus quod? Tempore facere dolorem fugiat?",
+  },
+  {
+    title: "title",
+    Subject:
+      "    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Itaque nisi tenetur maiores eos fugiat iste tempore delectus reprehenderit, ut ad! Harum culpa autem quae natus quod? Tempore facere dolorem fugiat?",
+  },
+  {
+    title: "title",
+    Subject:
+      "    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Itaque nisi tenetur maiores eos fugiat iste tempore delectus reprehenderit, ut ad! Harum culpa autem quae natus quod? Tempore facere dolorem fugiat?",
+  },
+  {
+    title: "title",
+    Subject:
+      "    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Itaque nisi tenetur maiores eos fugiat iste tempore delectus reprehenderit, ut ad! Harum culpa autem quae natus quod? Tempore facere dolorem fugiat?",
+  },
+  {
+    title: "title",
+    Subject:
+      "    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Itaque nisi tenetur maiores eos fugiat iste tempore delectus reprehenderit, ut ad! Harum culpa autem quae natus quod? Tempore facere dolorem fugiat?",
+  },
+  {
+    title: "title",
+    Subject:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Itaque nisi tenetur maiores eos fugiat iste tempore delectus reprehenderit, ut ad! Harum culpa autem quae natus quod? Tempore facere dolorem fugiat?",
+  },
+  {
+    title: "title",
+    Subject:
+      "    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Itaque nisi tenetur maiores eos fugiat iste tempore delectus reprehenderit, ut ad! Harum culpa autem quae natus quod? Tempore facere dolorem fugiat?",
+  },
+  {
+    title: "title",
+    Subject:
+      "    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Itaque nisi tenetur maiores eos fugiat iste tempore delectus reprehenderit, ut ad! Harum culpa autem quae natus quod? Tempore facere dolorem fugiat?",
+  },
+  {
+    title: "title",
+    Subject:
+      "    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Itaque nisi tenetur maiores eos fugiat iste tempore delectus reprehenderit, ut ad! Harum culpa autem quae natus quod? Tempore facere dolorem fugiat?",
+  },
+  {
+    title: "title",
+    Subject:
+      "    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Itaque nisi tenetur maiores eos fugiat iste tempore delectus reprehenderit, ut ad! Harum culpa autem quae natus quod? Tempore facere dolorem fugiat?",
+  },
+  {
+    title: "title",
+    Subject:
+      "    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Itaque nisi tenetur maiores eos fugiat iste tempore delectus reprehenderit, ut ad! Harum culpa autem quae natus quod? Tempore facere dolorem fugiat?",
+  },
+];
+
+for (let i = 0; i < titleAndDescription.length; i++) {
+  let div = document.createElement("div");
+  let totle = titleAndDescription[i];
+
+  let h1 = document.createElement("h1");
+  let p = document.createElement("p");
+
+  let texth1 = document.createTextNode(totle.title);
+  let textp = document.createTextNode(totle.Subject);
+
+  h1.appendChild(texth1);
+  p.appendChild(textp);
+
+  div.appendChild(h1);
+  div.appendChild(p);
+
+  document.body.appendChild(div);
+}
+
+setInterval(function printScrolllength() {
+    console.log(window.scrollY);
+  }, 1000);
