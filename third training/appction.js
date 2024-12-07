367a6fc219442c88236597803493a834ee53a6bd
Ibrahim-Karagaji <ibrahimKaragaji@gmail.com>
2024-12-07 23:04:37 +0300

    third training is done ( the third training was change color of the home page when the button is clicked)

--- third training/appction.js
+++ third training/appction.js
@@ -5,9 +5,11 @@ let colorsName = document.querySelector("h1");
 let buttonClick = document.querySelector("p");
 
 buttonClick.onclick = function () {
+
   const randomIndex = Math.floor(Math.random() * colors.length);
   const randomElement = colors[randomIndex];
   colorsName.innerText = randomElement.toString();
   buttonClick.style.backgroundColor = randomElement;
   document.body.style.backgroundColor = randomElement;
+  
 };
