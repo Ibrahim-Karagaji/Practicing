630cbce4c2ae60ec5acbfab498ce5874469ffee0
Ibrahim-Karagaji <ibrahimKaragaji@gmail.com>
2024-12-08 11:22:02 +0300

    creat function returns random word from the array

--- 4th-training/javascript.js
+++ 4th-training/javascript.js
@@ -2,3 +2,9 @@ const words = ["Great", "Good", "Bad", "Nice"];
 
 let p = document.querySelector("p");
 
+function getRandomWord() {
+  const randomIndex = Math.floor(Math.random() * words.length);
+  const randomElement = words[randomIndex];
+  return randomElement;
+}
+
