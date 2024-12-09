2d93f4811fa8a8661d1c9a6a02bedfb961868e85
Ibrahim-Karagaji <ibrahimKaragaji@gmail.com>
2024-12-10 00:10:57 +0300

    created function links up the element and the mouse

--- 5th-training/app.js
+++ 5th-training/app.js
@@ -1,1 +1,6 @@
 let div = document.querySelector("div");
+
+document.addEventListener("mousemove", function (event) {
+  div.style.left = -7 + event.clientX + "px";
+  div.style.top = -7 + event.clientY + "px";
+});
