f649ade9c3ade6a353b9165eca03c03a59b698f5
Ibrahim-Karagaji <ibrahimKaragaji@gmail.com>
2024-12-12 00:59:50 +0300

    add icon element at html file

--- 6th-ttraining/JAVASCRIPT.js
+++ 6th-ttraining/JAVASCRIPT.js
@@ -1,3 +1,6 @@
 let backGroundIMage = document.getElementById("backgroundID");
 let play = document.getElementById("play");
 
+play.onclick = function () {
+  backGroundIMage.style.opacity = "0%";
+};
