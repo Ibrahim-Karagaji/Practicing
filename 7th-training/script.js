52ded1d3638cba18538ada905c4a27a4b6e98fe2
Ibrahim-Karagaji <ibrahimKaragaji@gmail.com>
2024-12-13 01:29:04 +0300

    the 7th training is done (it was to switch color of the background when the button to the item)

--- 7th-training/script.js
+++ 7th-training/script.js
@@ -1,1 +1,17 @@
+let before = document.getElementById("before");
+let after = document.getElementById("after");
 
+after.onclick = function () {
+  after.classList.toggle("black");
+  if ((after.className == "black")) {
+    after.style.transform = "translate(63px, 0px)";
+    after.style.backgroundColor = "black";
+    before.style.backgroundColor = "white";
+    document.body.style.backgroundColor = "black";
+  } else {
+    after.style.transform = "translate(0px, 0px)";
+    after.style.backgroundColor = "white";
+    before.style.backgroundColor = "black";
+    document.body.style.backgroundColor = "white";
+  }
+};
